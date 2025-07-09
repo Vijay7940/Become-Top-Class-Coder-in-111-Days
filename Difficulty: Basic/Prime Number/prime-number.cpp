be8b3b00344cd6a72ff5e70ae7a029667e855e41
Vijay7940 <vijayjathare482@gmail.com>
2025-07-09 18:39:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Prime Number/prime-number.cpp
+++ Difficulty: Basic/Prime Number/prime-number.cpp
@@ -4,8 +4,13 @@ class Solution {
     bool isPrime(int n) {
         // code here
         if(n==1) return false;
-        for(int i=2;i<=sqrt(n);i++){
-	        if(n%i==0) return false;
+        
+        for(int i=2;i<n/2;i++){
+            
+	        if(n%i==0) {
+	            return false;
+	        }
+	        
 	    }
 	    return true;
     }
