e23f7e2a6dbe8593861d125ea86b6eeb25387d0a
Vijay7940 <vijayjathare482@gmail.com>
2025-07-09 18:32:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Leap Year/leap-year.java
+++ Difficulty: Basic/Leap Year/leap-year.java
@@ -3,9 +3,9 @@
 class Solution {
     static boolean isLeap(int N) {
         // code here
-        if(N%100==0 && N%400==0) //century year
+        if(N % 100 ==0 && N % 400 == 0) //century year
             return true;
-        else if(N%100!=0 && N%4==0) //normal year
+        else if(N % 100 != 0 && N %4 == 0) //normal year
             return true;
         else 
             return false;
