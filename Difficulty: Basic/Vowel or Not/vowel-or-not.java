30c994dceecd2dee0214b0da7473889265f046e2
Vijay7940 <vijayjathare482@gmail.com>
2025-07-09 19:14:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Vowel or Not/vowel-or-not.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+class Solution {
+    String isVowel(char c) {
+        // code here
+        if (c == 'A' || c == 'a' || c == 'E' || c == 'e' || 
+            c == 'I' || c == 'i' || c == 'O' || c == 'o' || 
+            c == 'U' || c == 'u') 
+        {
+            return "YES";
+        }
+        else{
+            return "NO";
+        }
+    }
+}
